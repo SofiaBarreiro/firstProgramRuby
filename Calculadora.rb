9283df509761307aaf4cf7b9b60b86b131d32911
unknown <usuario@NOT00236.ministerio.trabajo.gov.ar>
2020-12-25 19:01:19 -0300

    commit

--- Calculadora.rb
+++ Calculadora.rb
@@ -23,17 +23,13 @@ $variable #las variables fuera de las clases van con $
 #sin signo las variables son locales
 # con @ las variables son de instancia de la clase
 #para calcular un exponente se debe poner ** ej. 2**3  es dos a la tres
-
 #para multiplicar una cadena se puede usar el signo *
-
 #para insertar un simbolo se puede poner el codigo ascii ej. cadena.concat(nroascii) tambien se puede usar el <<
-
-
 #para medir la cadena cadena.lenght
-
 #para comparar dos cadenas <=> compara cual es la mas grande
-
 #casecmp se usa para comparar si las cadenas son distintas iguales, devuelve true o false
+#capitalize para poner la primera letra en mayuscula
+
 
 
 def MostrarMenu()
@@ -44,7 +40,62 @@ def MostrarMenu()
 
     hola = "hola" << " y chau"
 
-    print hola
+    #print hola
+
+
+   # hola.each_char{|c| print c
+
+    #print "letra\n"
+    #todo lo que va a aca es parte del each, se puede agregar codigo    
+    #con \n hago el salto d linea
+
+    #}
+    #hola = hola.center(40)
+    #print hola  #la funcion center lo que hace 
+    #es extender la cantidad de caracteres de un string. si tiene menos queda igual, si tiene mas lo que hace es agregar espacios al rededor. Se lepuede agregar un texto para que aparezca a los costados
+    hola = hola.center(20, "-----")
+    print hola << "\n"
+
+    nro = 12
+
+    if nro > 20
+
+        #puts "es otra forma de imprimir texto"
+
+
+
+    else
+
+         puts "pasa por else"
+
+    end
+    if not nro > 20
+
+        puts "para negar se utiliza la palabra not"
+        puts "para and  se utiliza la palabra and"
+
+        #nro1 == 1 and nro2 == 2 
+
+        #tambien se puede usar unless que es lo mismo que if not
+
+        puts "para or  se utiliza la palabra or"
+
+         #nro1 == 1 or nro2 == 2 
+
+
+    end
+
+
+
+    #operador when es similar al switch case
+    edad = 2
+    case edad
+    when 0..11 then print "esta entre 0 y 11"
+    when 12..22 then print "esta entre 12 y 22"
+    #para finalizar el case uso un end
+    else 
+        print "hace otra cosa"
+    end
 
     gets()
 
